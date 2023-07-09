4fdf4124febef940b92000a1df2421a0bb980e68
Samip Pokharel <callmesamip@gmail.com>
2023-07-09 20:26:58 +1000

    Implementing Grafbase

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 const Home = () => {
   return (
-    <div>
-      <div className="flex-start flex-col padding mb-6">
-        <h1>Categories</h1>
-        <h1>Posts</h1>
-        <h1>More</h1>
-      </div>
-    </div>
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>More</h1>
+    </section>
   );
 };
 
