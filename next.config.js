3a3470da3c544f6fee31200f7216d32bd1f7cd07
Samip Pokharel <callmesamip@gmail.com>
2023-07-16 16:20:25 +1000

    Update Config to ignore TS errors

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     },
     experimental: {
         serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     }
 };
 
